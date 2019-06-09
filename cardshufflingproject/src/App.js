0cedd44cca3e00e7ab61d24f123467e2d8fbe191
rheechashaipu <rheechashaipu@gmail.com>
2019-06-09 15:09:57 -0400

    Add readme.

---
+++ cardshufflingproject/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DisplayCards from './components/DisplayCards.js';
+
+function App() {
+  return (
+    <div className="App">
+      <DisplayCards />
+    </div>
+  );
+}
+
+export default App;
