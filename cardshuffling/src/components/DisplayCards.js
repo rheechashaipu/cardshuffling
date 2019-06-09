4a8ac68905b75e248ef67cab1568ed3280966706
rheechashaipu <rheechashaipu@gmail.com>
2019-06-09 13:26:05 -0400

    Add reactstrap and bootstrap packages. Check to see if I am rendering 52 cards.

--- cardshuffling/src/components/DisplayCards.js
+++ cardshuffling/src/components/DisplayCards.js
@@ -8,9 +8,14 @@ class DisplayCards extends Component {
 
         //Check to see that 52 unique cards are logged when the page renders
         console.log(StandardDeck);
+        let displayedDeck = StandardDeck.map((card, index) =>
+            <div key={index}>
+                {card.house} {card.rank}
+            </div>
+        )
         return(
             <div>
-
+                {displayedDeck}
             </div>
         );
     }
